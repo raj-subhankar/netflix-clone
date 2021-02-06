4a6345e8396de7e8fda231de3f42b8f9e086ea84
Subhankar Raj <subhankar.rj@gmail.com>
2021-02-06 23:00:51 +0530

    Create Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homescreen from './Homescreen';
 
 function App() {
   return (
     <div className="app">
-      <h1>Netflix</h1>
+      <Homescreen />
     </div>
   );
 }
---
+++ src/Homescreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Homescreen.css'
+import Nav from './Nav'
+
+function Homescreen() {
+    return(
+        <div className='homescreen'>
+            <Nav />
+        </div>
+    )
+}
+
+export default Homescreen
