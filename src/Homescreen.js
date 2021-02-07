e40249bf2037069b7e4232c1fe846d26a6b784ce
Subhankar Raj <subhankar.rj@gmail.com>
2021-02-07 12:29:40 +0530

    Create Row

--- src/Homescreen.js
+++ src/Homescreen.js
@@ -2,12 +2,47 @@ import React from 'react'
 import Banner from './Banner'
 import './Homescreen.css'
 import Nav from './Nav'
+import requests from './Requests'
+import Row from './Row'
 
 function Homescreen() {
     return(
         <div className='homescreen'>
             <Nav />
             <Banner />
+
+            <Row 
+                title="NETFLIX ORIGINALS" 
+                fetchUrl={requests.fetchNetflixOriginals} 
+                isLargeRow />
+            
+            <Row 
+                title="Trending Now"
+                fetchUrl={requests.fetchTrending} />
+
+            <Row 
+                title="Top Rate"
+                fetchUrl={requests.fetchTopRated} />
+
+            <Row
+                title="Action Movies"
+                fetchUrl={requests.fetchActionMovies} />
+
+            <Row
+                title="Comedy Movies"
+                fetchUrl={requests.fetchComedyMovies} />
+
+            <Row    
+                title="Horror Movies"
+                fetchUrl={requests.fetchHorrorMovies} />
+
+            <Row
+                title="Romance Movies"
+                fetchUrl={requests.fetchRomanceMovies} />
+
+            <Row
+                title="Documentaries"
+                fetchUrl={requests.fetchDocumentaries} />
         </div>
     )
 }
