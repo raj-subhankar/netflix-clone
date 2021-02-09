a88b4f77dd1fb5ec1ad864a4a420f52138558535
Subhankar Raj <subhankar.rj@gmail.com>
2021-02-09 20:59:50 +0530

    Implement Stripe checkout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import Homescreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
 
-  const user = null;
+  const dispatch = useDispatch()
+  const user = useSelector(selectUser)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    return unsubscribe
+  }, [dispatch])
 
   return (
     <div className="app">
@@ -21,8 +41,12 @@ function App() {
           <LoginScreen />
         ) : (
         <Switch>
+          <Route path="/profile">
+            <ProfileScreen />
+          </Route>
+
           <Route path="/">
-            <Homescreen />
+            <HomeScreen />
           </Route>
         </Switch>
         )}
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import './Nav.css'
 
 function Nav() {
 
     const [show, handleShow] = useState(false)
+    const history = useHistory()
 
     const transitionNavBar = () => {
         if(window.scrollY > 100) {
@@ -22,11 +24,13 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
                 <img 
+                    onClick={()=> history.push('/')}
                     className="nav__logo" 
                     src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
                     alt='netflix logo'  />
 
                 <img 
+                    onClick={() => history.push('/profile')}
                     className="nav__avatar" 
                     src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"/>
             </div>
